2d67d213113d7f10b5d00da50f0e6c7d7272a559
Antonio Tornero <antoniotornero@Antonios-MacBook-Pro.local>
2023-10-07 10:08:56 +0100

    first commit

---
+++ src/Node.java
@@ -0,0 +1,43 @@
+
+public class Node {
+	int id;
+	int X;
+	int Y;
+	
+	public Node(int id, int X, int Y) {
+		this.id = id;
+		this.X = X;
+		this.Y = Y;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getX() {
+		return X;
+	}
+	public void setX(int x) {
+		X = x;
+	}
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+	
+	public double euclidianDistance(Node nodeB) {
+	    double distX = Y - nodeB.getY();
+	    double distY = X - nodeB.getX();
+	    return Math.sqrt(distX*distX + distY*distY);
+	}
+	
+	@Override
+	public String toString() {
+		return "Node [id=" + id + ", X=" + X + ", Y=" + Y + "]";
+	}
+}
---
+++ src/TSPMain.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class TSPMain {
+
+	public static ArrayList<Node> readFile(String fileName) {
+
+		ArrayList<Node> nodeArr = new ArrayList<>();
+		try {
+			File myObj = new File(fileName);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+				List<String> nodeData = Arrays.asList(data.trim().split(" "));
+				nodeArr.add(new Node(Integer.valueOf(nodeData.get(0)), Integer.valueOf(nodeData.get(1)),
+						Integer.valueOf(nodeData.get(2))));
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return nodeArr;
+	}
+	
+	public static void NearNeighbour(ArrayList<Node> nodes) {
+		 Node firstNode =nodes.remove(0);
+		 Double closestNodeDistance;
+		 Node nextNode = firstNode;
+		 Node tempNode = null;
+		 Double totalDistance = 0.0;
+		 StringBuffer stringBuffer = new StringBuffer();
+		 stringBuffer.append(firstNode.getId() + "->");
+		 
+		 while(!nodes.isEmpty()) {
+			 closestNodeDistance = Double.MAX_VALUE;
+			 for(Node possibleNode : nodes) {
+				 if(nextNode.euclidianDistance(possibleNode) < closestNodeDistance) {
+					 tempNode = possibleNode;
+				 }
+			 }
+			 
+			 totalDistance = totalDistance + nextNode.euclidianDistance(tempNode);
+			 stringBuffer.append(tempNode.getId() + "->");
+			 nextNode = tempNode;
+			 nodes.remove(nextNode);
+		 }
+		 
+		 stringBuffer.append(firstNode.getId());
+		 totalDistance = totalDistance + nextNode.euclidianDistance(firstNode);
+		 
+		System.out.println(stringBuffer.toString() +"\nTotal Distance: " + totalDistance );
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Node> nodeArr =readFile("resources/train1.txt");
+		NearNeighbour(nodeArr);
+//		for(Node e : nodeArr) {
+//			System.out.println(e.toString());
+//		}
+
+	}
+}
